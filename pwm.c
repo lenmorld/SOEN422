0463b7b37dd2c7a43bc304ffabd4879bc1fa0310
root <root@beaglebone.localdomain>
2015-11-03 22:17:30 +0000

    finished lab5, part1

--- pwm.c
+++ pwm.c
@@ -10,42 +10,52 @@ int main(void) {
 slots = fopen("/sys/devices/bone_capemgr.9/slots", "w");
 
 fprintf(slots, "am33xx_pwm");
-
+fflush(slots);
 fprintf(slots, "bonw_pwm_P9_14");
+fflush(slots);
 
 fclose(slots);
 
 
 
 period = fopen("/sys/devices/ocp.3/pwm_test_P9_14.22/period", "w");
-duty = fopen("/sys/devices/ocp.3/pwm_test_P9_14.22/duty", "w");
 run = fopen("/sys/devices/ocp.3/pwm_test_P9_14.22/run", "w");
+
+duty = fopen("/sys/devices/ocp.3/pwm_test_P9_14.22/duty", "w");
 
 
 fprintf(period, "400000");
+fflush(period);
+fclose(period);
+
+
 int intensity = 0;
+
 fprintf(duty, "0");
+fflush(duty);
+fclose(duty);
 
 fprintf(run, "1");
-
-char buffer[7];
+fflush(run);
+fclose(run);
 
 
 while (1) {
-	if (intensity > 300000) {
-		intensity += 1;
+
+	duty = fopen("/sys/devices/ocp.3/pwm_test_P9_14.22/duty", "w");
+	
+	if (intensity < 320000) {
+		intensity += 80000;
 	} else {
 		intensity = 0;
 	}
-	sprintf(buffer, "%d", intensity);
-	fprintf(duty, "%s", buffer);
-	//usleep(1 * 1000);
+	fprintf(duty, "%d", intensity);
+	fflush(duty);
+	fclose(duty);
+	usleep(500000);
 }
 
 
-fclose(period);
-fclose(duty);
-fclose(run);
 
 return 0;
 }
